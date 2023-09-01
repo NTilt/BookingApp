269e8c1e76efcdc325459d61782b2744db035b04
Nikita <ya.n.2@mail.com>
2023-09-01 15:59:42 +0400

    init state

---
+++ BookingApp/BookingApp/App/BookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingAppApp.swift
+//  BookingApp
+//
+//  Created by Никита Ясеник on 01.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
